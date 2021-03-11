ddfdc0c636f72af1e6d688a7e1aa313eb0eece9f
BoyanPeychinov <BoyanPeychinov@gmail.com>
2021-03-11 17:18:34 +0200

    Problem 01 of inheritance - exercise

---
+++ inheritance/exercise/problem_01/child.py
@@ -0,0 +1,5 @@
+from problem_01.person import Person
+
+
+class Child(Person):
+    pass
---
+++ inheritance/exercise/problem_01/test.py
@@ -0,0 +1,9 @@
+from problem_01.child import Child
+from problem_01.person import Person
+
+person = Person("Peter", 25)
+child = Child("Peter Junior", 5)
+print(person.name)
+print(person.age)
+print(child.__class__.__bases__[0].__name__)
+print(child.__class__.__bases__)
